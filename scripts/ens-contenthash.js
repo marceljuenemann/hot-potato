e265aa2206dc7f95b8d8c78e26e68e05d570ddf5
Marcel Juenemann <mail@marcel-juenemann.de>
2026-02-07 15:01:00 +0000

    Final touches

--- scripts/ens-contenthash.js
+++ scripts/ens-contenthash.js
@@ -1,6 +1,6 @@
 import { decode, encode } from "@ensdomains/content-hash";
 
-const ipfs = "bafybeif7g3sma2a2mjatfsfodylnkbsdr5xhiy27tiavwri5qygz7lv4ei";
+const ipfs = "bafybeibiiicnjtdd63enq4awu3ww65bdsgphb5ejr5exgc77yf5wcv2gjm";
 const encoded = encode("ipfs", ipfs);
 
 console.log(encoded);
